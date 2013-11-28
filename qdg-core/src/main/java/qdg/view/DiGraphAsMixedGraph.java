b1fbff74d3489e5c318eb958421aaf0bad566312
Marton Makai <marton.makai@gmail.com>
2013-11-28 19:01:56 +0100

    View directed and undirected graphs as mixed graphs.

---
+++ qdg-core/src/main/java/qdg/view/DiGraphAsMixedGraph.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2013 Marton Makai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package qdg.view;
+
+import java.util.Iterator;
+
+import com.google.common.collect.Iterators;
+
+import qdg.EntityMaps.NullMap;
+import qdg.api.DiGraph;
+import qdg.api.EntityMap;
+import qdg.api.bits.EdgeMutationHandler;
+import qdg.api.bits.NodeMutationHandler;
+import qdg.bits.AbstractMixedGraph;
+
+public class DiGraphAsMixedGraph extends AbstractMixedGraph {
+
+	protected DiGraph graph;
+	
+	public DiGraphAsMixedGraph(DiGraph graph) {
+		this.graph = graph;
+	}
+	
+	@Override
+	public boolean isDirected(Edge edge) {
+		return true;
+	}
+
+	@Override
+	public Node addNode() {
+		return graph.addNode();
+	}
+
+	@Override
+	public void remove(Node n) {
+		graph.remove(n);
+	}
+
+	@Override
+	public Edge addUEdge(Node source, Node target) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void remove(Edge edge) {
+		graph.remove(edge);
+	}
+
+	@Override
+	public Node getSource(Edge edge) {
+		return graph.getSource(edge);
+	}
+
+	@Override
+	public Node getTarget(Edge edge) {
+		return graph.getTarget(edge);
+	}
+
+	@Override
+	public Iterator<Node> getNodeIterator() {
+		return graph.getNodeIterator();
+	}
+
+	@Override
+	public <V> EntityMap<Node, V> createNodeMap() {
+		return graph.createNodeMap();
+	}
+
+	@Override
+	public <V> EntityMap<Edge, V> createEdgeMap() {
+		return graph.createEdgeMap();
+	}
+
+	@Override
+	public Iterator<Edge> getUEdgeIterator() {
+		return Iterators.emptyIterator();
+	}
+
+	@Override
+	public Iterator<Edge> getIncidentUEdgeIterator(Node node) {
+		return Iterators.emptyIterator();
+	}
+
+	@Override
+	public <V> EntityMap<Edge, V> createUEdgeMap() {
+		return new NullMap<Edge, V>();
+	}
+
+	@Override
+	public void addNodeMutationHandler(NodeMutationHandler handler) {
+		graph.addNodeMutationHandler(handler);
+	}
+
+	@Override
+	public void addEdgeMutationHandler(EdgeMutationHandler handler) {
+		graph.addEdgeMutationHandler(handler);
+	}
+
+	@Override
+	public Edge addArc(Node source, Node target) {
+		return graph.addArc(source, target);
+	}
+
+	@Override
+	public Iterator<Edge> getArcIterator() {
+		return graph.getArcIterator();
+	}
+
+	@Override
+	public Iterator<Edge> getOutArcIterator(Node node) {
+		return graph.getOutArcIterator(node);
+	}
+
+	@Override
+	public Iterator<Edge> getInArcIterator(Node node) {
+		return graph.getInArcIterator(node);
+	}
+
+	@Override
+	public Iterator<Edge> getIncidentArcIterator(Node node) {
+		return graph.getIncidentArcIterator(node);
+	}
+
+	@Override
+	public <V> EntityMap<Edge, V> createArcMap() {
+		return graph.createArcMap();
+	}
+}
---
+++ qdg-core/src/main/java/qdg/view/UGraphAsMixedGraph.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2013 Marton Makai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package qdg.view;
+
+import java.util.Iterator;
+
+import com.google.common.collect.Iterators;
+
+import qdg.EntityMaps.NullMap;
+import qdg.api.EntityMap;
+import qdg.api.UGraph;
+import qdg.api.bits.EdgeMutationHandler;
+import qdg.api.bits.NodeMutationHandler;
+import qdg.bits.AbstractMixedGraph;
+
+public class UGraphAsMixedGraph extends AbstractMixedGraph {
+
+	protected UGraph graph;
+	
+	public UGraphAsMixedGraph(UGraph graph) {
+		this.graph = graph;
+	}
+	
+	@Override
+	public boolean isDirected(Edge edge) {
+		return false;
+	}
+
+	@Override
+	public Node addNode() {
+		return graph.addNode();
+	}
+
+	@Override
+	public void remove(Node n) {
+		graph.remove(n);
+	}
+
+	@Override
+	public Edge addUEdge(Node source, Node target) {
+		return graph.addUEdge(source, target);
+	}
+
+	@Override
+	public void remove(Edge edge) {
+		graph.remove(edge);
+	}
+
+	@Override
+	public Node getSource(Edge edge) {
+		return graph.getSource(edge);
+	}
+
+	@Override
+	public Node getTarget(Edge edge) {
+		return graph.getTarget(edge);
+	}
+
+	@Override
+	public Iterator<Node> getNodeIterator() {
+		return graph.getNodeIterator();
+	}
+
+	@Override
+	public <V> EntityMap<Node, V> createNodeMap() {
+		return graph.createNodeMap();
+	}
+
+	@Override
+	public <V> EntityMap<Edge, V> createEdgeMap() {
+		return graph.createEdgeMap();
+	}
+
+	@Override
+	public Iterator<Edge> getUEdgeIterator() {
+		return graph.getUEdgeIterator();
+	}
+
+	@Override
+	public Iterator<Edge> getIncidentUEdgeIterator(Node node) {
+		return graph.getIncidentUEdgeIterator(node);
+	}
+
+	@Override
+	public <V> EntityMap<Edge, V> createUEdgeMap() {
+		return graph.createUEdgeMap();
+	}
+
+	@Override
+	public void addNodeMutationHandler(NodeMutationHandler handler) {
+		graph.addNodeMutationHandler(handler);
+	}
+
+	@Override
+	public void addEdgeMutationHandler(EdgeMutationHandler handler) {
+		graph.addEdgeMutationHandler(handler);
+	}
+
+	@Override
+	public Edge addArc(Node source, Node target) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterator<Edge> getArcIterator() {
+		return Iterators.emptyIterator();
+	}
+
+	@Override
+	public Iterator<Edge> getOutArcIterator(Node node) {
+		return Iterators.emptyIterator();
+	}
+
+	@Override
+	public Iterator<Edge> getInArcIterator(Node node) {
+		return Iterators.emptyIterator();
+	}
+
+	@Override
+	public Iterator<Edge> getIncidentArcIterator(Node node) {
+		return Iterators.emptyIterator();
+	}
+
+	@Override
+	public <V> EntityMap<Edge, V> createArcMap() {
+		return new NullMap<Edge, V>();
+	}
+}
